b0e5104b58df78e97c2113b3d1664d47aa2f5958
sihyeon-park0527 <psh6285@naver.com>
2026-05-14 14:12:44 +0900

    feat : 버튼2 facility 컨트롤러 추가

--- src/main/java/kr/hcnc/web/MainController.java
+++ src/main/java/kr/hcnc/web/MainController.java
@@ -1,14 +1,9 @@
 package kr.hcnc.web;
-
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import javax.annotation.Resource;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import kr.hcnc.service.DormitoriesService;
 
 @Controller
@@ -19,18 +14,48 @@ public class MainController {
 	
 	@RequestMapping(value = "/main.do")
 	public String mainPage() {
-		
 		System.out.println("MainController :: /main.do");
-		
-		List<Map<String, Object>> list = dormitoriesService.selectTest();
-		
-		for(Map<String, Object> map : list) {
-			System.out.println(map);
-		}
-		
-		System.out.println("Exit For state");
-		
 		return "main/main";
 	}
+	// 버튼 1
+	@RequestMapping(value = "/badge.do")
+	public String badgePage() {
+		System.out.println("MainController :: /badge.do");
+		return "badge/badgePage";
+	}
 	
-}
+	// 버튼 2 (보변적 시설 및 정보 안내)
+	@RequestMapping(value = "/facility.do")
+	public String facilityPage() {
+		System.out.println("MainController :: /facility.do");
+		return "facility/facilityPage";
+	}
+	
+	// 버튼 2 (구내식당 및 위치 정보)
+	@RequestMapping(value = "/facility/cafeteria.do")
+	public String cafeteriaPage() {
+		System.out.println("MainController :: /facility/cafeteria.do");
+		return "facility/cafeteriaPage";
+	}
+	
+	// 버튼 2 (흡연장소 위치 정보)
+	@RequestMapping(value = "/facility/smoking.do")
+	public String smokingPage() {
+		System.out.println("MainController :: /facility/somking.do");
+		return "facility/smokingPage";
+	}
+	
+	// 버튼 2 (강의실 위치 정보)
+	@RequestMapping(value = "/facility/classroom.do")
+	public String classroomPage() {
+		System.out.println("MainController :: /facility/classroom.do");
+		return "facilty/classroomPage";
+	}
+	
+	// 버튼 2 (교통 정보)
+	@RequestMapping(value = "facility/transport.do")
+	public String transportPage() {
+		System.out.println("MainController :: /facility/transport.do");
+		return "facility/transportPage";
+	}	
+}	
